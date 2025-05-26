e770c744281969f99217cade54f6ba416dd4c92f
Yanzz231 <mrpartys05@gmail.com>
2025-05-26 03:59:42 +0100

    fix: function.js

--- src/function/function.js
+++ src/function/function.js
@@ -1,7 +1,7 @@
 export const isDaytime = () => {
   const hour = new Date().getHours();
   // return hour >= 7 && hour < 18;
-  return false
+  return true
 };
 
 export const quizData = (id, path) => {
